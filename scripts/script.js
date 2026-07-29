5ab5e5180e63030b2d817ac96e1110ca1cb6b2e1
TimelessGX <144057631+ReimuDuck@users.noreply.github.com>
2026-07-29 16:34:27 -0700

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
-let testPara = document.querySelector(".flavor");
+const testPara = document.querySelector(".flavor");
+const hint1 = document.querySelector(".hint1");
+const hint2 = document.querySelector(".hint2");
+const hint3 = document.querySelector(".hint3");
+const hint4 = document.querySelector(".hint4");
+const input = document.getElementById("guess");
+const winORlose = document.querySelector(".winORlose");
+guesses = 4;
+correct = false;
 
 async function fetchJSONData() {
   try {
@@ -19,11 +27,10 @@ function getTodayString() {
 }
 async function arrayStateCheck(){
     const challArray = await fetchJSONData();
-    const today = new Date();
     var firstDay = new Date(2026,6,29,0);
     // const arrayDates = [];
     for (let i = 0; i < challArray.length; i++) {
-        if (today.toISOString().split('T')[0] == firstDay.toISOString().split('T')[0]){
+        if (getTodayString() == firstDay.toISOString().split('T')[0]){
             return i;
         }
         firstDay = new Date(firstDay.getTime());
@@ -40,21 +47,78 @@ async function getDailyChallenge(){
     console.log(challArray[challToGive]);
     return(challArray[challToGive]);
 }
+function nextHint(){
+    switch(guesses){
+        case 4:
+            hint1.classList.remove("hint1");
+            guesses -= 1;
+            break;
+        case 3:
+            hint2.classList.remove("hint2");
+            guesses -= 1;
+            break;
+        case 2:
+            hint3.classList.remove("hint3");
+            guesses -= 1;
+            break;
+        case 1:
+            hint4.classList.remove("hint4");
+            guesses -= 1;
+            break;
+        default:
+            break;
+    }
+}
+function verify(array){
+    const form = input.elements.guessBox.value;
+    if(form == array.name){
+        correct = true;
+        winORlose.textContent = "WINNER IS YOU!";
+        winORlose.classList.remove("winORlose");
+        return -1;
+    }else if(form != array.name && 0 < guesses){
+        nextHint();
+    }else{
+        winORlose.textContent = "LOSER IS YOU!";
+        winORlose.classList.remove("winORlose");
+    }
+}
+
+
+
+function setChallHints(arrChall){
+    hint1.textContent = "HP: " + arrChall.hp;
+    hint2.textContent = "Type(s): " + arrChall.types;
+    hint3.textContent = "Stage:" + arrChall.stage;
+    hint4.textContent = "Dex Number: " + arrChall.dexNumber;
+}
+
+
+
 // getDailyChallenge();
 // Function to check if we need to run today's action
 async function runDailyTask() {
     try {
         const challDone = false;
         const todaysArray = await getDailyChallenge();
+        const handler = () => {
+            event.preventDefault();
+            verify(todaysArray);
+
+            if (correct || guesses <= 0) {
+                input.removeEventListener("input", handler);
+            }
+        };
+        input.addEventListener("submit", handler);
+
         if (!todaysArray) {
             console.error("Could not load JSON.");
             return;
         }
         testPara.textContent = todaysArray.description;
+        setChallHints(todaysArray);
         if (challDone == false) {
             
-
-            // Save today's date so it won't run again until tomorrow
             // localStorage.setItem('lastDailyRun', today);
 
         } else {
